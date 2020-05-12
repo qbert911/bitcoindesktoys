39c789acc4473071f0be4021d91b828d60efb4e0
Ben <yurgartisb@gmail.com>
2020-05-12 14:01:06 -0400

    foo

--- piglow_speed.py
+++ piglow_speed.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 import sys
-from piglow import PiGlow
+import piglow
 
-piglow = PiGlow()
+piglow.clear_on_exit = False
+
+#piglow = piglow()
 vala = min(11, int(sys.argv[1]))
 piglow.all(0)
 for colour in range(0,6):
     if vala > colour and vala < colour + 7:
         piglow.colour(6-colour, 1)
+piglow.show()
--- stop.sh
+++ stop.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 /home/pi/bitcoin/bitcoin-cli -datadir=/home/pi/bitcoin stop
+tail -n 20 -f /home/pi/bitcoin/debug.log
