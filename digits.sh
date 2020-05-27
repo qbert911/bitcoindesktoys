7a400854c6f76d629a0f30b9088adab486100b3f
qbert911 <yurgartisb@gmail.com>
2020-05-27 14:30:42 -0400

    add unicorn bars function

--- digits.sh
+++ digits.sh
@@ -23,9 +23,10 @@ while : ;do
       change=$(( $changeup < -10 ? -10 : $changeup ))
     fi
     if [[ "$lastreading" = "0" ]];then lastreading=$usdreading;START=$(($START+10));fi  #make first row behave properly
-    #eval "/home/pi/bitcoindesktoys/show_digitsmove.py $lastreading $usdreading $change"
     eval "/home/pi/bitcoindesktoys/write_history.py $usdreading"
-    echo -en "\n\$$usdreading $(printf '%+03d' $change) $(printf '%+04d' $(( $usdreading - $lastreading )) )\$ change  $(( $(date +%s) - $START )) seconds"
+    eval "/home/pi/bitcoindesktoys/show_digitsmove.py $lastreading $usdreading $change"
+    eval "sudo /home/pi/bitcoindesktoys/unicorn_bars.py"
+    echo -en "\$$usdreading $(printf '%+03d' $change) $(printf '%+04d' $(( $usdreading - $lastreading )) )\$ change  $(( $(date +%s) - $START )) seconds"
     START=$(date +%s)
   fi
   lastreading=$usdreading
--- unicorn_bars.py
+++ unicorn_bars.py
@@ -1,10 +1,22 @@
 #!/usr/bin/env python
 """
-blah
+unicorn hat - show price movement bar chart
 """
+# pylint: disable=C0103,C0301
+from __future__ import print_function
 import json
 import os
 import sys
+import unicornhat as unicorn
+
+unicorn.set_layout(unicorn.AUTO)
+unicorn.rotation(0)
+with open(os.devnull, 'w') as f:   #to prevent loading message
+    oldstdout = sys.stdout
+    sys.stdout = f
+    unicorn.brightness(0.2)  #prints warning
+    sys.stdout = oldstdout
+
 
 def main():
     """asdasd"""
@@ -12,7 +24,7 @@ def main():
     file_name = "/home/pi/history.json"
 
     if not os.path.isfile(file_name):  #instantiate new config file
-        mydict = {"history":[9001,9002,9003,9004,9005,9006,9007,9008]}
+        mydict = {"history":[9001, 9002, 9003, 9004, 9005, 9006, 9007, 9008]}
         with open(file_name, "w") as outfile:
             json.dump(mydict, outfile)
     else:
@@ -21,29 +33,35 @@ def main():
 
     myrange = max(myfile["history"])-min(myfile["history"])
     myfloor = min(myfile["history"])
-    print ( "minimum is:", min(myfile["history"]), " maximum is: ",max(myfile["history"]) )
-    print "range: ", myrange
+    print("minimum is:", min(myfile["history"]), " maximum is: ", max(myfile["history"]), "range: ", myrange)
     print(myfile["history"])
 
-    for y in range(0,8):
-        checkpoint = myfloor+((y)*myrange/8)
+    for y in range(0, 8):
+        checkpoint = myfloor+((y)*myrange/8)+1
         checkpointb = myfloor+((y+1)*myrange/8)
-        print checkpoint,checkpointb
+        print(checkpoint, "-", checkpointb, " ", end='')
 
-    for x in range(0,8):
-        print(x,myfile["history"][x], myfile["history"][x+1]),
-        for y in range(0,8):
-            checkpoint = myfloor+((y)*myrange/8)
+    print("")
+    for x in range(0, 8):
+        print(x, myfile["history"][x], myfile["history"][x+1], " ", end='')
+        for y in range(0, 8):
+            checkpoint = myfloor+((y)*myrange/8)+1
             checkpointb = myfloor+((y+1)*myrange/8)
-            if min(myfile["history"][x], myfile["history"][x+1]) <= checkpointb and max(myfile["history"][x], myfile["history"][x+1]) >= checkpoint:
+            if min(myfile["history"][x], myfile["history"][x+1]) < checkpointb and max(myfile["history"][x], myfile["history"][x+1]) >= checkpoint:
                 if myfile["history"][x] < myfile["history"][x+1]:
-                    print "o",
+                    print("o ", end='')
+                    unicorn.set_pixel(x, 7-y, 0, 255, 0)
                 else:
-                    print "x",
+                    print("x ", end='')
+                    unicorn.set_pixel(x, 7-y, 255, 0, 0)
             else:
-                print ".",
-        print ""
+                print(". ", end='')
+                unicorn.set_pixel(x, 7-y, 0, 0, 0)
+        print("")
 
+    unicorn.show()
 
+    #time.sleep(10)
 if __name__ == "__main__":
-      main()
+    main()
+    unicorn.atexit._exithandlers = []
