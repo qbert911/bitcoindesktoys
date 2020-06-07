b2b48d811dc3498a3c0aae7c27428facf3055b89
qbert911 <yurgartisb@gmail.com>
2020-06-06 21:50:42 -0400

    ubbb

--- unicorn_bars.py
+++ unicorn_bars.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-unicorn hat - show price movement bar charts
+unicorn hat - show scatter plot of price history
 """
 # pylint: disable=C0103,C0301
 from __future__ import print_function
@@ -13,47 +13,73 @@ unicorn.rotation(0)
 with open(os.devnull, 'w') as f:   #to prevent loading message
     oldstdout = sys.stdout
     sys.stdout = f
-    unicorn.brightness(0.2)  #prints warning
+    unicorn.brightness(.3)  #prints warning
     sys.stdout = oldstdout
 
 def main():
     """asdasd"""
 
     file_name = "/home/pi/history.json"
-
     if not os.path.isfile(file_name):  #instantiate new config file
         mydict = {"history":[9001, 9002, 9003, 9004, 9005, 9006, 9007, 9008, 9009]}
         with open(file_name, "w") as outfile:
             json.dump(mydict, outfile)
+
     with open(file_name, 'r') as openfile:
         myfile = json.load(openfile)
 
+    try:
+        hist_chunk_size = int(sys.argv[1])
+    except:
+        hist_chunk_size = len(myfile["history"])/8
+    #unicorn.clear()
     myfloor = 9999999
     myceiling = 0
-    for x in range(len(myfile["history"])-9, len(myfile["history"])):
+    hist_offset = len(myfile["history"]) - 1 - (hist_chunk_size * 8)
+    for x in range(hist_offset, len(myfile["history"])):
         myfloor = min(myfloor, myfile["history"][x])
         myceiling = max(myceiling, myfile["history"][x])
     myrange = myceiling - myfloor
-    print("   ("+str(myfloor)+" - "+str(myceiling)+")   range:", myrange, "(", (myrange/8.00), "per cell )")
 
-    for x in range(len(myfile["history"])-9, len(myfile["history"])-1):
-        print(x-(len(myfile["history"])-9), x, myfile["history"][x], myfile["history"][x+1], "{:+04d}".format(myfile["history"][x+1] - myfile["history"][x]), " ", end='')
-        for y in range(0, 8):
+    print("   ("+str(myfloor)+" - "+str(myceiling)+")   range:", myrange, "(", (myrange/8.00), "per cell )    ", len(myfile["history"]), "history records ", hist_chunk_size, " history per cell    offset:", hist_offset)
+
+    for x in range(0, 8):
+        print(myfile["history"][(x*hist_chunk_size)+hist_offset], myfile["history"][((x+1)*hist_chunk_size)+hist_offset], (x*hist_chunk_size)+hist_offset, ((x+1)*hist_chunk_size)+hist_offset, end='  ')
+    print("")
+
+    for y in range(7, -1, -1):
+        for x in range(0, 8):
+            #print(myfile["history"][(x*hist_chunk_size)+hist_offset], myfile["history"][((x+1)*hist_chunk_size)+hist_offset-1],(x*hist_chunk_size)+hist_offset,((x+1)*hist_chunk_size)+hist_offset,end='')
+            localmin = 9999999
+            localmax = 0
+            for histloop in range(0, hist_chunk_size):
+                localmin = min(myfile["history"][(x*hist_chunk_size)+histloop+hist_offset], localmin)
+                localmax = max(myfile["history"][(x*hist_chunk_size)+histloop+hist_offset], localmax)
+
+            #print(localmin,localmax,end='')
             checkpoint = myfloor+((y)*myrange/8.00)
             checkpointb = myfloor+((y+1)*myrange/8.00)
-            if min(myfile["history"][x], myfile["history"][x+1]) <= checkpointb and max(myfile["history"][x], myfile["history"][x+1]) > checkpoint:
-                if myfile["history"][x] < myfile["history"][x+1]:
+            if min(myfile["history"][(x*hist_chunk_size)+hist_offset], myfile["history"][((x+1)*hist_chunk_size)+hist_offset]) <= checkpointb and \
+               max(myfile["history"][(x*hist_chunk_size)+hist_offset], myfile["history"][((x+1)*hist_chunk_size)+hist_offset]) > checkpoint:
+                if myfile["history"][(x*hist_chunk_size)+hist_offset] < myfile["history"][((x+1)*hist_chunk_size)+hist_offset]:
+                    print("O ", end='')
+                    unicorn.set_pixel(x, 7-y, 0, 180, 0)
+                else:
+                    print("X ", end='')
+                    unicorn.set_pixel(x, 7-y, 180, 0, 0)
+
+            elif localmin <= checkpointb and localmax > checkpoint:
+                if myfile["history"][(x*hist_chunk_size)+hist_offset] < myfile["history"][((x+1)*hist_chunk_size)+hist_offset]:
                     print("o ", end='')
-                    unicorn.set_pixel(x-(len(myfile["history"])-9), 7-y, 0, 255, 0)
+                    unicorn.set_pixel(x, 7-y, 150, 150, 150)
                 else:
                     print("x ", end='')
-                    unicorn.set_pixel(x-(len(myfile["history"])-9), 7-y, 255, 0, 0)
+                    unicorn.set_pixel(x, 7-y, 150, 150, 150)
 
             else:
                 print(". ", end='')
-                unicorn.set_pixel(x-(len(myfile["history"])-9), 7-y, 0, 0, 0)
+                #unicorn.set_pixel(x/20, 7-y, 0, 0, 0)
         print("")
-
     unicorn.show()
 
 if __name__ == "__main__":
