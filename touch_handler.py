6dbdd607854dd8e4a88183b87eba0aaedebf5537
qbert911 <yurgartisb@gmail.com>
2020-07-24 13:33:11 -0400

    set touch handling flag to global

--- touch_handler.py
+++ touch_handler.py
@@ -13,27 +13,32 @@ file_name = "/home/pi/trigger.foo"
 
 @rainbowhat.touch.A.press()
 def touch_a(channel):
+    global flag
+    flag = 1
     if config_filefunctions.is_unicornhat_inverted() > -1:
         config_filefunctions.zoom_level_up()
         rainbowhat.lights.rgb(1, 0, 0)
-        flag=1
 
 @rainbowhat.touch.B.press()
 def touch_b(channel):
+    global flag
+    flag = 1
     if config_filefunctions.is_unicornhat_inverted() > -1:
         config_filefunctions.zoom_level_down()
         rainbowhat.lights.rgb(0, 1, 0)
-        flag=1
 
 @rainbowhat.touch.C.press()
 def touch_c(channel):
+    global flag
+    flag = 0
     config_filefunctions.flip_sound()
     rainbowhat.lights.rgb(0, 0, 1)
-    flag=0
 
 @rainbowhat.touch.release()
 def release(channel):
+    global flag
     rainbowhat.lights.rgb(0, 0, 0)
+    print (flag)
     if flag and config_filefunctions.is_unicornhat_inverted() > -1:
         if not os.path.exists(file_name):
             unicorn_bars_calculate.ubars_write()
