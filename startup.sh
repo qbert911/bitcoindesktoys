6f09bd69e0d04e202b806906e99bb26b086ff07d
qbert911 <yurgartisb@gmail.com>
2020-08-21 13:48:26 -0400

    switch to startup bash script instead of rc.local

--- startup.sh
+++ startup.sh
@@ -1,1 +1,15 @@
-/home/pi/Pimoroni/unicornhathd/examples/demo.py &
+#!/bin/bash
+#/home/pi/Pimoroni/unicornhathd/examples/demo.py &
+sudo /home/pi/bitcoindesktoys/touch_handler.py &
+sudo /home/pi/bitcoindesktoys/unicorn_handler.py &
+
+( sudo /etc/init.d/ntp stop
+until ping -nq -c3 8.8.8.8; do
+   echo "Waiting for network..."
+done
+ntpdate -s time.nist.gov
+sudo /etc/init.d/ntp start )&
+
+#until nc -zw1 1.1.1.1 443 &>/dev/null;do sleep 3;done
+#sleep 25
+#sudo /home/pi/bitcoin/bitcoind -datadir=/home/pi/bitcoin &
