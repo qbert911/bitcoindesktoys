b78c4d3dbb0be4912cbafd299b4c944ab8e9138d
qbert911 <yurgartisb@gmail.com>
2020-08-21 13:52:39 -0400

    use ntpd instead

--- startup.sh
+++ startup.sh
@@ -7,7 +7,7 @@ sudo /home/pi/bitcoindesktoys/unicorn_handler.py &
 until ping -nq -c3 8.8.8.8; do
    echo "Waiting for network..."
 done
-ntpdate -s time.nist.gov
+sudo ntpd -gq
 sudo /etc/init.d/ntp start )&
 
 #until nc -zw1 1.1.1.1 443 &>/dev/null;do sleep 3;done
