9621b239e0e8fdfa88e5c2c1cdc2e7b12a9b76a0
qbert911 <yurgartisb@gmail.com>
2020-08-25 10:36:10 -0400

    major digits overhaul

--- digits.sh
+++ digits.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # shellcheck disable=SC2004
 #eval 'ulimit -S -s 16384' #to help prevent segfault errors when running show_digitsmove
-lastreading=$((0))
 change=$((-1))
 hasunicornhat=$(cat /home/pi/config.json | jq '.invert_unicornhat')
 echo -e "Watching BTC price movements..."
@@ -12,25 +11,35 @@ while : ;do
     if [ -z "$usdreading" ] || [[ "$usdreading" = "null" ]] || [[ "$usdreading" = "0" ]];then usdreading=$lastreading;fi #in case of timeout
     usdreading=$(printf '%04.0f' "$usdreading")
   else  usdreading=$lastreading;  echo -en "x";sleep 30; fi #data scrape unsuccessful
-
-  if [[ "$usdreading" = "$lastreading" ]];then    #price hasnt changed
-    if [[ "$dotcounter" -gt "1" ]];then
+  timer=$(( $(date +%_S) + 1 ))
+  if ( ( [[ "$timer" -le "55" ]] || [[ "$evernew" -eq "0" ]] ) && ( [[ "$usdreading" = "$LASTREPORTED" ]] || [[ "$usdreading" = "$SecLASTREPORTED" ]] ) ) || ( [[ "$timer" -le "55" ]] && [[ ${#LASTREPORTED} > 1 ]] );then    #price hasnt changed
+    if [[ "$usdreading" = "$LASTREPORTED" ]];then         #same value detected
+      LASTSAME=$(date +%X)
       echo -en "."
-      dotcounter=$((0))
+    elif [[ "$usdreading" = "$SecLASTREPORTED" ]];then    #older value detected
+      echo -en "p"
+    else                                                  #new value detected
+      echo -en "n"
+      if [[ "$evernew" -eq "0" ]];then
+        FOUND=$(date +%s)
+        evernew=$((1))
+        NEWVALUE=$usdreading
+      fi
     fi
-  else
-    if [[ "$usdreading" > "$lastreading" ]];then
+  else #time to report a change in price
+    if [[ "$evernew" -eq "0" ]];then FOUND=$(date +%s);else usdreading=$NEWVALUE;evernew=$((0));fi
+    if [[ "$usdreading" > "$LASTREPORTED" ]];then
       changeup=$(($change+1))
       change=$(( $changeup > 10 ? 10 : $changeup ))
     else
       changeup=$(($change-1))
       change=$(( $changeup < -10 ? -10 : $changeup ))
     fi
-    if [[ "$lastreading" = "0" ]];then lastreading=$usdreading  #make first row behave properly
-    else echo -e "$(( $(date +%s) - $START )) seconds] $(date +%X)";fi
+    if [[ "${#LASTREPORTED}" = "0" ]];then LASTREPORTED=$usdreading  #make first row behave properly
+    else echo -e "$(( $FOUND - $START )) seconds] ($LASTSAME)   $SecLASTREPORTED $LASTREPORTED $usdreading $evernew";fi
     START=$(date +%s)
-    while [[ "$(( $(date +%_S) + 1 ))" -ne "60" ]];do sleep 0.05;done #sync multiple units
-    eval "/home/pi/bitcoindesktoys/show_digitsmove.py $lastreading $usdreading $change" &
+    while [[ "$(( $(date +%_S) + 1 ))" -ne "60" ]] && [[ ${#SecLASTREPORTED} > 1 ]];do sleep 0.05;done #sync multiple units
+    eval "/home/pi/bitcoindesktoys/show_digitsmove.py $LASTREPORTED $usdreading $change" &
     if [[ "$hasunicornhat" -ge "0" ]] && [[ "$usdreading" -ge "10" ]]; then
       eval "/home/pi/bitcoindesktoys/write_history.py $usdreading"
       eval "sudo /home/pi/bitcoindesktoys/unicorn_bars_calculate.py 1"
@@ -41,12 +50,11 @@ while : ;do
     else
       sleep 10
     fi
-    echo -en "\$$usdreading $(printf '%+03d' $change) $(printf '%+04d' $(( $usdreading - $lastreading )) )\$ ["
-    sleep 40
-    dotcounter=$((0))
+    echo -en "\$$usdreading $(printf '%+03d' $change) $(printf '%+04d' $(( $usdreading - $LASTREPORTED )) )\$ ["
+    SecLASTREPORTED=$LASTREPORTED
+    LASTREPORTED=$usdreading
   fi
   lastreading=$usdreading
-  dotcounter=$(($dotcounter+1))
   sleep 1
   while [[ "$(( $(date +%s) % 5 ))" -ne "0" ]];do sleep 0.5;done
 done
